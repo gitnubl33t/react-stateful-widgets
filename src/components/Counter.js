428c78dbb95fe1081461f56c3206f2fedfa9bf62
gitnubl33t <jessicad8695@gmail.com>
2020-08-20 20:16:49 -0700

    finished step 3, counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -71,7 +71,10 @@ export default function Counter() {
   return (
     <div className="widget-counter container">
       <h2>Counter</h2>
-      <div style={style}>Number 0 is even</div> {/* STEP 3 */}
+      <div style={style}>
+        Number {count} is {count % 2 === 0 ? "even" : "odd"}
+      </div>
+      {/* STEP 3  */}
       <div>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
